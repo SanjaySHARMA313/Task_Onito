3ab45995506206432169b9657d96aa91f330d02f
SanjaySHARMA313 <sanjay.1802305034@sanskriti.ac.in>
2022-08-01 17:41:17 +0530

    Upload to github

--- src/Forms.js
+++ src/Forms.js
@@ -2,79 +2,76 @@ import {React,useState} from 'react'
 import css from './Forms.css'
 
 const Forms = () => {
-    const[Amount,setAmount]=useState('');
-    const [Remark,setRemark]=useState('');
-    const [Date,setDate]=useState("");
-    const[Option,setOption]=useState("Cash");
+  
+    const[FirstName,setFirstName]=useState("");
+    const[LastName,setLastName]=useState("");
+     const[City,setCity]=useState("");
+    const[Country,setCountry]=useState("");
     const[Data,setData]=useState([]);
-    const click=()=>{
-        if(Amount && Date && Option){
+    const click=(shouldAddAtStart)=>{
+        if(FirstName && LastName && City && Country){
 
-        let Newent={Amount:Amount,Remark:Remark,Date:Date,Payment:Option};
-             setData([...Data,Newent]);
-             setAmount("");
-             setRemark("");
-             setDate("");
-             setOption("Cash");
-            console.log(Newent);
+    
+        let NewEntry={FirstName:FirstName,LastName:LastName,City:City,Country:Country};
+         if(shouldAddAtStart){
+            setData([NewEntry,...Data]);
+            }
+            else{
+                setData([...Data,NewEntry]);}
+                setFirstName("");
+                setLastName("");
+                setCity("");
+                setCountry("");
+            
             }
          else{
             alert("please fill the Details")
         }
     }
-    const cancel=()=>{
-        setAmount("");
-        setRemark("");
-        setDate("01/01/2022");
-        setOption("Cash")
-    }
+ 
   return (
     <div className ="form">
 
 <form onSubmit={(e)=>e.preventDefault()} className="forms" >
-<h3 className='header'>Receipt Details</h3>  
-        <label className='date' > Date *</label>
-        <input type="Date" placeholder="Enter Date" className='dates'  value={Date} onChange={(e)=>setDate(e.target.value)}/><br/><br/><br/>
-        <label className='amount' >Amount *</label>
-        <input type="number" placeholder="Enter Amount (in INR)" className='amounts' value={Amount} onChange={(e)=>setAmount(e.target.value)}/><br/><br/><br/>
-        <label className='payment'>Payment Mode</label>
-         <select className='select' onChange={(e)=>setOption(e.target.value)} ><option>Cash</option>
-         <option value="cheque" >Cheque</option>
-         <option value="Net Banking">Net Banking</option>
-         <option  value="UPI"> UPI</option>
-             </select>
+
+        <label className='fnamee' > First_Name</label>
+        <input type="text" placeholder=" Ex. John" className='fname'  value={FirstName} onChange={(e)=>setFirstName(e.target.value)}/><br/><br/><br/>
+        <label className='lnamee' >Last_Name</label>
+        <input type="text" placeholder=" Ex. Sharma" className='lname' value={LastName} onChange={(e)=>setLastName(e.target.value)}/><br/><br/><br/>
+        <label className='cityy'>City</label>
+        <input type="text" onChange={(e)=>setCity(e.target.value)} placeholder=" Ex. Delhi" value={City} className="city"></input>
         
              <br/><br/><br/>
-        <label className='remark'  >Remark</label>
-        <input type="text" placeholder="Enter Remark" className='remarks'  value={Remark} onChange={(e)=>setRemark(e.target.value)}></input>
+        <label className='countryy'  >Country</label>
+        <input type="text" placeholder="Ex. India" className='country'  value={Country} onChange={(e)=>setCountry(e.target.value)}></input>
         <div className='button'>
-             <button className='cancel' onClick={cancel}>CANCEL<br/>(ESC)</button>
-         <button className='submit' onClick={click}>SUBMIT <br/>(Ctrl + s)</button>
+             <button className='add' onClick={click.bind(this,true)}>Add_First</button>
+         <button className='submit' onClick={click.bind(this,false)}>ADD_Last</button>
          </div>
            </form>
          
          {
             Data && Data.length>0 && (
-                <table className="showDataa" >
-                    <thead >
-                        <tr className='showData' >
-                            <td>S.NO.</td>
-                            <td>Date</td>
-                            <td> Amount</td>
-                            <td>Payment </td>
-                            <td >Remark</td>
+                <table className="table">
+                    <thead  className='tablee'>
+                        <tr className="tableee">
+                            <td className="tables">S.NO.</td>
+                            <td className="tables">FirstName</td>
+                            <td className="tables">lastName</td>
+                            <td className="tables">City </td>
+                            <td className="tables">Country</td>
                         </tr>
                     </thead>
                     <tbody>
                    { 
                         Data.map((curelm,id)=>{
                             return(
-                                <tr key={id} className="showData" >
-                                    <td>{id+1}</td>
-                                    <td>{curelm.Date}</td>
-                                    <td> {curelm.Amount}</td>
-                                    <td>{curelm.Payment}</td>
-                                    <td>{curelm.Remark}</td>
+                                <tr key={id} className="tables">
+                                    <td className="tables">{id+1}</td>
+                                    <td className="tables">{curelm.FirstName}</td>
+                                    <td className="tables">{curelm.LastName}</td>
+                                    <td className="tables">{curelm.City}</td>
+                                    <td className="tables">{curelm.Country}</td>
                                 </tr>
                
                             )
@@ -94,3 +91,4 @@ const Forms = () => {
 }
 
 export default Forms
+
